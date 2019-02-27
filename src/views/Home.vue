44b93c0a0d1ecb64706dc1d9819cb8857b3a6af7
Anel Rojas <anel@dino-it.com>
2019-02-27 16:28:39 -0600

    adding vexflow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,14 @@
     <NoteToolkit/>
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <keyboard msg="Music Thingy"/>
+    <div id="boo"></div>
   </div>
 </template>
 
 <script>
 import NoteToolkit from '@/components/NoteToolkit.vue';
-
 import keyboard from '@/components/keyboard.vue';
+// import Vex from 'vexflow';
 
 export default {
   name: 'home',
