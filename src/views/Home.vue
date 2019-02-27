d6933c6ab345ddb1ecbbd5362ed8a32d04594644
Bruno Sanchez <bruno@dino-it.com>
2019-02-27 15:53:47 -0600

    Add keyboard component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
     <NoteToolkit/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <keyboard msg="Music Thingy"/>
   </div>
 </template>
 
 <script>
 import NoteToolkit from '@/components/NoteToolkit.vue';
 
+import keyboard from '@/components/keyboard.vue';
+
 export default {
   name: 'home',
   components: {
     NoteToolkit,
+    keyboard,
   },
 };
 </script>
