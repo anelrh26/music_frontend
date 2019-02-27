c98b1b9436707b966d2628253951991e010047be
Anel Rojas <anel@dino-it.com>
2019-02-27 12:54:30 -0600

    adding element-ui dependency to have nice ui components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'reset-css/reset.css';
+import 'chartist/dist/scss/chartist.scss';
+
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
+Vue.use(require('vue-chartist'));
 
 new Vue({
   router,
