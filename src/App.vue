b7e8cde82b090df1e10e8b41c96d7431402c5cb4
Anel Rojas <anel@dino-it.com>
2019-02-27 22:17:01 -0600

    adding styles to the menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
-    <el-menu theme="dark" class="el-menu-demo" mode="horizontal" :router="true">
+    <el-menu
+      theme="dark"
+      class="el-menu-demo"
+      mode="horizontal"
+      :router="true"
+      :default-active="activeIndex"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
       <el-menu-item index="1" :route="{ name: 'home' }">Home</el-menu-item>
       <el-menu-item index="2" :route="{ name: 'about' }">About</el-menu-item>
     </el-menu>
-    <router-view/>
+    <router-view id="content"/>
   </div>
 </template>
 
@@ -12,6 +21,11 @@
 <script>
 export default {
   name: 'home',
+  data() {
+    return {
+      activeIndex: '1',
+    };
+  },
 };
 </script>
 
@@ -22,6 +36,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#content {
+  margin-top: 40px;
 }
 /*
 #nav {
